2d9a6fecc0948d2b009bee0491312cc47dbab25f
RabahCodeVirtuose <rabah.toubal.etudes@gmail.com>
2025-11-18 11:19:32 +0100

    creation du fichier d'initialisation et de la creation de la base de données et ajout de dépendances

---
+++ explorez_votre_ville/lib/db/database_helper.dart
@@ -0,0 +1,79 @@
+// lib/db/database_helper.dart
+
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  // singleton : une seule instance dans toute l'application
+  DatabaseHelper._internal();
+  static final DatabaseHelper instance = DatabaseHelper._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  // Ouverture / création physique du fichier.db de la BDD
+  Future<Database> _initDatabase() async {
+    final documentDir = await getApplicationDocumentsDirectory();
+    final dbPath = p.join(documentDir.path, 'explorez_ville.db');
+
+    return await openDatabase(
+      dbPath,
+      version: 1,
+      onConfigure: _onConfigure,
+      onCreate: _onCreate,
+    );
+  }
+
+  //Activation des clés étrangères (important pour on DELETE CASCADE)
+  Future<void> _onConfigure(Database db) async {
+    await db.execute('PRAGMA foreign_keys = ON');
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    // Table VILLE
+    await db.execute('''
+      CREATE TABLE ville (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nom TEXT NOT NULL,
+        pays TEXT,
+        latitude REAL,
+        longitude REAL,
+        is_favorie INTEGER NOT NULL DEFAULT 0,
+        is_visitee INTEGER NOT NULL DEFAULT 0,
+        is_exploree INTEGER NOT NULL DEFAULT 0
+      );
+    ''');
+    // Table LIEU
+    await db.execute('''
+      CREATE TABLE lieu (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nom TEXT NOT NULL,
+        type TEXT NOT NULL,
+        description TEXT,
+        latitude REAL,
+        longitude REAL,
+        image_path TEXT,
+        ville_id INTEGER NOT NULL,
+        FOREIGN KEY(ville_id) REFERENCES ville(id) ON DELETE CASCADE
+      );
+    ''');
+
+    // Table COMMENTAIRE
+    await db.execute('''
+      CREATE TABLE commentaire (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        contenu TEXT,
+        note INTEGER NOT NULL,
+        created_at TEXT NOT NULL,
+        lieu_id INTEGER NOT NULL,
+        FOREIGN KEY(lieu_id) REFERENCES lieu(id) ON DELETE CASCADE
+      );
+    ''');
+  }
+}
